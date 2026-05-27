46ca976220ffcb589da52155bfda2f72b29f9021
Shadorux <yomazre@gmail.com>
2026-05-27 18:49:01 -0400

    Remove batch file

--- push_to_github.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d c:\Users\ggsmo\OneDrive\Desktop\shadowtranslator
-git init
-git add .
-git commit -m "Initial Shadow Translator upload"
-git branch -M main
-git remote add origin https://github.com/Shadorux/ultimate-lifeform-translator.git
-git push -u origin main
-pause
